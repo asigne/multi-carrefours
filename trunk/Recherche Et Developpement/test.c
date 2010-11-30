c8e3a84e9cb46b38002eda149178bd86ac4fdc0e
aurelien.signe@gmail.com <aurelien.signe@gmail.com@6351359c-2329-7bd8-614f-23122de92eea>
2010-11-30 14:30:56 +0000

    Souci dans la fonction gestionCarrefour du à l'écrasement des voitures en memoire par les nouvelles arrivees

--- trunk/Recherche Et Developpement/test.c
+++ trunk/Recherche Et Developpement/test.c
@@ -1,117 +1,112 @@
 #include "fonctions.h"
 
+
+
 int main(int argc, char **argv)
 {
-	int msgid;
-	if((msgid = msgget(666, IPC_CREAT | IPC_EXCL | 0666)) == -1){
-		//perror(strerror(errno));
-	}
-	
-	int pid = fork();
+	int i;
+	msgid[0]= msgget(666, IPC_CREAT | IPC_EXCL | 0666);
+	msgid[1]= msgget(667, IPC_CREAT | IPC_EXCL | 0666);
+	msgid[2]= msgget(668, IPC_CREAT | IPC_EXCL | 0666);
+	msgid[3]= msgget(669, IPC_CREAT | IPC_EXCL | 0666);
 
-	if(pid == 0)
-	{
-		// processus gérant le carrefour 1 (NORD-OUEST)
- 		int ligne,colonne;
-		key_t clef;
-		int semid,cpt=0;
-		afficheEtatSem();	//Afficher l'état des sémaphores
-		//créer les 36 sémaphores du carrefour
-		for(ligne = 0; ligne<6; ligne++){
-			for(colonne = 0; colonne<6; colonne++){	
-				clef = ftok(argv[0], ID_PROJET+cpt); //verifier unicité des clefs générée
-				sem_in_out[ligne][colonne]=creerSem(clef);
-				initSem(sem_in_out[ligne][colonne], 1);
-				cpt++;		
-			}
+	pid_t pidPere=getpid();
+	for(i=0;i<4;i++){
+		if(getpid()==pidPere){
+			//printf("Creation du fils numero %d\n",i);
+			pidCarrefour[i] = fork();
 		}
-		
-		pthread_t thread_traitement[3];
-		
-		while(1)
-		{
-			// attendre voiture
-			printf("Attente de voiture...\n");
-			mess* m = (mess*) malloc(sizeof(mess));
-			msgrcv(msgid, m, sizeof(mess), 0, 0);
-			// file de la voiture = m.type
-			m->car.entree = m->type-1;
-			
-			// créer thread traiter voiture.
-			
-			//pthread_t thread_traitement;
-			pthread_create(&thread_traitement[m->type-1], NULL, (void * (*)(void *))traitement, m);
-			
-			//usleep(100);
-		}
-		//destruction semaphores du carrefour
-		destructionSem();
+	}
+
+	if(pidCarrefour[0] == 0){
+		// processus gérant le carrefour 0 (NORD-OUEST)
+		gestionCarrefour(0);
+		printf("Mort du fils 0...\n");
+		exit(0);
+	}
+	else if(pidCarrefour[1] == 0){
+		// processus gérant le carrefour 1 (NORD-EST)
+	//	gestionCarrefour(1);
+		printf("Mort du fils 1...\n");		
+		exit(0);		
+	}
+	else if(pidCarrefour[2] == 0){
+		// processus gérant le carrefour 1 (NORD-EST)
+	//	gestionCarrefour(2);
+		printf("Mort du fils 2...\n");		
+		exit(0);		
+	}
+	else if(pidCarrefour[3] == 0){
+		// processus gérant le carrefour 1 (NORD-EST)
+	//	gestionCarrefour(3);
+		printf("Mort du fils 3...\n");		
+		exit(0);		
 	}
 	else{
 		// pere
-		mess m;
-		m.car.id = 11;
-		m.car.sortie = EN_FACE;
-		m.car.entree = -1;
-		m.type = 1; //OUEST
-		
 		mess m1;
-		m1.car.id = 15;
-		m1.car.sortie = GAUCHE;
-		m1.car.entree = -1;
-		m1.type = 2; //SUD
+		m1.car.id = 1;
+		m1.car.sortie = EST;
+		m1.car.entree = OUEST;
+		m1.type = OUEST;
 		
 		mess m2;
-		m2.car.id = 12;
-		m2.car.sortie = GAUCHE;
-		m2.car.entree = -1;
-		m2.type = 3; //EST
+		m2.car.id = 2;
+		m2.car.sortie = NORD;
+		m2.car.entree = OUEST;
+		m2.type = OUEST;
 		
+		mess m3;
+		m3.car.id = 3;
+		m3.car.sortie = SUD;
+		m3.car.entree = OUEST;
+		m3.type = OUEST;
+
+
+		mess m4;
+		m4.car.id = 4;
+		m4.car.sortie = EST;
+		m4.car.entree = OUEST;
+		m4.type = OUEST;
 		
-		msgsnd(msgid, &m, sizeof(mess) - sizeof(long), 0);
-		msgsnd(msgid, &m2, sizeof(mess) - sizeof(long), 0);
-		msgsnd(msgid, &m1, sizeof(mess) - sizeof(long), 0);	
+		mess m5;
+		m5.car.id = 5;
+		m5.car.sortie = NORD;
+		m5.car.entree = OUEST;
+		m5.type = OUEST;
+		
+		mess m6;
+		m6.car.id = 6;
+		m6.car.sortie = SUD;
+		m6.car.entree = OUEST;
+		m6.type = OUEST;
+
+
+		
+		msgsnd(msgid[0], &m1, sizeof(mess) - sizeof(long), 0);	
+		msgsnd(msgid[0], &m2, sizeof(mess) - sizeof(long), 0);
+		msgsnd(msgid[0], &m3, sizeof(mess) - sizeof(long), 0);
+		msgsnd(msgid[0], &m4, sizeof(mess) - sizeof(long), 0);	
+	//	msgsnd(msgid[0], &m5, sizeof(mess) - sizeof(long), 0);
+	//	msgsnd(msgid[0], &m6, sizeof(mess) - sizeof(long), 0);
 		//printf("2 Nouvelle voiture dans la file...\n");
+
+
+	/*	for(i=0;i<4;i++){
+			printf("%d\n",pidCarrefour[i]);
+		}*/
 		
-		wait();
-		
-		printf("Fils mort...\n");
+		waitpid(pidCarrefour[0], NULL, NULL);
+		waitpid(pidCarrefour[1], NULL, NULL);
+		waitpid(pidCarrefour[2], NULL, NULL);
+		waitpid(pidCarrefour[3], NULL, NULL);
+
+		printf("Mort du père...\n");
 	}
 }
 
 
-void traitement(mess* v1)
-{
-	voiture* v = &(v1->car);
-	switch(v->sortie)
-	{
-		// v->entree numéro de l'entrée
-		case DROITE:
-		{
-			printf("Entree dans le carrefour voiture %d par l'entree %d.\n", v->id, v->entree);			
-			tourneDroite(*v);
-			printf("Voiture id:%d, sort  carrefour !\n", v->id);
-			break;
-		}
-		case EN_FACE:
-		{
-			printf("Entree dans le carrefour voiture %d par l'entree %d.\n", v->id, v->entree);
-			enFace(*v);
-			printf("Voiture id:%d, sort  carrefour !\n", v->id);
-			break;
-		}
-		case GAUCHE:
-		{
-			printf("Entree dans le carrefour voiture %d par l'entree %d.\n", v->id, v->entree);
-			tourneGauche(*v);
-			printf("Voiture id:%d, sort  carrefour !\n", v->id);
-			break;
-		}
-	}	
-	free(v1);
-	
-	pthread_exit(0);
-}
+
 
 
 
