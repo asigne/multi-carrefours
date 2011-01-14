68ae43f57bcb75fb63d02b2de7834de2ebc5e78f
aurelien.signe <aurelien.signe@6351359c-2329-7bd8-614f-23122de92eea>
2011-01-14 12:00:16 +0000

    version opérationnelle V3

--- trunk/Recherche Et Developpement/parametrage.h
+++ trunk/Recherche Et Developpement/parametrage.h
@@ -1,12 +1,12 @@
 //constante pour le programme
 #define ID_PROJET 'P'
-#define NbVoituresGlobal 0				// nombre de voitures deja sur le circuit au demarrage
+#define NbVoituresGlobal 100				// nombre de voitures deja sur le circuit au demarrage
 #define ProbabiliteVehiculePrioritaire 50 	//une chance sur X
 #define MAX_TRAFFIC 15						//nombre de vehicules maximum dans une file "inter-carrefour"
 #define delaisNouvelleVoiture 10000//10000			//delais entre la creation de 2 nouvelles voitures (en nanoSecondes)
 #define tpsPourTraiterUneVoiture 31000//40000		//temps pour traiter une voiture
 
-#define pourcentageRemplissageFile 80		//pourcentage de remplissage a partir de laquelle la file se vide
+#define pourcentageRemplissageFile 90		//pourcentage de remplissage a partir de laquelle la file se vide
 
 
 
